6d4106ee29bcf6729f5f15a30f1f0c60ec725b09
d1d2c0d1 <d1d2c0d1@gmail.com>
2021-10-06 02:55:43 +0300

    MT-1015 Create UserHistory model

---
+++ app/Models/UserHistory.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+/**
+ * Class UserHistory
+ * @package App\Models
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $role_id
+ * @property integer $permission_id
+ * @property integer $status
+ * @property string $additional
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class UserHistory extends Model
+{
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_history';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The table date format
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+        'role_id',
+        'permission_id',
+        'status',
+        'additional',
+
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'role_id' => 'integer',
+        'permission_id' => 'integer',
+        'status' => 'integer',
+        'additional' => 'string',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+
+    ];
+
+
+    /**
+     * Relationship for connection with User model (table)
+     *
+     * @return BelongsTo
+     */
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
