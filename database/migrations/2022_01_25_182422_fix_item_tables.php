a827ddbdf477436ec1090a24186cda5da4e0dd15
d1d2c0d1 <d1d2c0d1@gmail.com>
2022-01-25 21:36:47 +0300

    Fix migration

--- database/migrations/2022_01_25_182422_fix_item_tables.php
+++ database/migrations/2022_01_25_182422_fix_item_tables.php
@@ -14,6 +14,8 @@ class FixItemTables extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
+            $table->dropForeign('items_fk9');
+            $table->dropForeign('items_fk10');
             $table->dropIndex('items_category_id_index');
             $table->dropIndex('items_price_type_id_index');
         });
