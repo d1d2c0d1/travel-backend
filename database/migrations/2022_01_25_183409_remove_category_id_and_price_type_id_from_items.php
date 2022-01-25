f0cd542cea50881d7ae7d5bd1d55b3a468ef440e
d1d2c0d1 <d1d2c0d1@gmail.com>
2022-01-25 21:34:43 +0300

    Add migration

--- database/migrations/2022_01_25_183409_remove_category_id_and_price_type_id_from_items.php
+++ database/migrations/2022_01_25_183409_remove_category_id_and_price_type_id_from_items.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FixItemTables extends Migration
+class RemoveCategoryIdAndPriceTypeIdFromItems extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,10 @@ class FixItemTables extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->dropIndex('items_category_id_index');
-            $table->dropIndex('items_price_type_id_index');
+            $table->removeColumn('category_id');
+            $table->removeColumn('price_type_id');
+            $table->dropColumn('category_id');
+            $table->dropColumn('price_type_id');
         });
     }
 
@@ -26,6 +28,8 @@ class FixItemTables extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('items', function (Blueprint $table) {
+            //
+        });
     }
 }
