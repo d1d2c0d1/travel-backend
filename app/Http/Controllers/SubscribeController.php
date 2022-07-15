c94d487e426790246d71a0aafca8a8bd7135acd4
Stanislav Strizhkov <stanislav.strizhkov@idefa.ru>
2022-07-15 16:45:17 +0300

    Fix status for subscriptions routes

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Subscribe;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SubscribeController extends Controller
 {
@@ -12,9 +13,9 @@ class SubscribeController extends Controller
      * Display a listing of the resource.
      *
      * @param Request $request
-     * @return array
+     * @return Response
      */
-    public function index(Request $request): array
+    public function index(Request $request): Response
     {
         $email = (string) $request->json('email');
 
@@ -22,49 +23,49 @@ class SubscribeController extends Controller
          * Email validation
          */
         if (mb_strlen($email) <= 10) {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'Email too short.',
-            ];
+            ], 405);
         }
 
         $emailChunks = explode('@', $email);
 
         if (count($emailChunks) !== 2) {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'In email has not found @',
-            ];
+            ], 405);
         }
 
         if (mb_strlen($emailChunks[0]) <= 4) {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'Email name is very too short.',
-            ];
+            ], 405);
         }
 
         if (mb_strpos($emailChunks[1], '.') === false) {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'In email has not found dot',
-            ];
+            ], 405);
         }
 
         $domainChunks = explode('.', $emailChunks[1]);
 
         if (mb_strlen($domainChunks[0]) < 2) {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'Domain name in email is very too short.',
-            ];
+            ], 405);
         }
 
         if (mb_strlen($domainChunks[1]) < 2) {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'Domain zone in email is very too short.',
-            ];
+            ], 405);
         }
 
         /**
@@ -78,23 +79,23 @@ class SubscribeController extends Controller
         try {
             $subscribe->save();
         } catch (QueryException $e) {
-            return [
+            return response([
                 'status' => false,
                 'error' => "Duplicate entry '{$email}'.",
-            ];
+            ], 500);
         }
 
         if ($subscribe->wasRecentlyCreated) {
-            return [
+            return response([
                 'status' => true,
                 'subscribeId' => $subscribe->id,
-            ];
+            ]);
 
         } else {
-            return [
+            return response([
                 'status' => false,
                 'error' => 'Failed to create table row.'
-            ];
+            ], 500);
 
         }
     }
